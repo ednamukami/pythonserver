c6cb6c32d3d899405f271ec9bf534cd10f93948a
student <ednnamukami@gmail.com>
2021-07-08 19:40:11 +0300

    first commit

---
+++ student/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Student(models.Model):
+    first_name=models.CharField(max_length=12)
+    last_name=models.CharField(max_length=12)
+    age=models.PositiveSmallIntegerField()
+    date_of_birth=models.DateField()
